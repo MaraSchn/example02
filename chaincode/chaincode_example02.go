6681921dcecfaf1b930d4d41418220ac4657ef89
Maraike Schneider <maraikeschneider@MaraikesMacBook.fritz.box>
2017-07-01 16:48:21 +0200

    Changed Init Msg

--- chaincode/chaincode_example02.go
+++ chaincode/chaincode_example02.go
@@ -32,8 +32,8 @@ type SimpleChaincode struct {
 }
 
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	fmt.Printf("Init called, initializing chaincode")
-	
+	fmt.Printf("Maraike changed this: Init called, initializing chaincode")
+
 	var A, B string    // Entities
 	var Aval, Bval int // Asset holdings
 	var err error
@@ -72,7 +72,7 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 // Transaction makes payment of X units from A to B
 func (t *SimpleChaincode) invoke(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	fmt.Printf("Running invoke")
-	
+
 	var A, B string    // Entities
 	var Aval, Bval int // Asset holdings
 	var X int          // Transaction value
@@ -128,7 +128,7 @@ func (t *SimpleChaincode) invoke(stub *shim.ChaincodeStub, args []string) ([]byt
 // Deletes an entity from state
 func (t *SimpleChaincode) delete(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	fmt.Printf("Running delete")
-	
+
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 3")
 	}
@@ -148,7 +148,7 @@ func (t *SimpleChaincode) delete(stub *shim.ChaincodeStub, args []string) ([]byt
 // This chaincode will manage two accounts A and B and will transfer X units from A to B upon invoke
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Printf("Invoke called, determining function")
-	
+
 	// Handle different functions
 	if function == "invoke" {
 		// Transaction makes payment of X units from A to B
@@ -168,7 +168,7 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 
 func (t* SimpleChaincode) Run(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Printf("Run called, passing through to Invoke (same function)")
-	
+
 	// Handle different functions
 	if function == "invoke" {
 		// Transaction makes payment of X units from A to B
@@ -189,7 +189,7 @@ func (t* SimpleChaincode) Run(stub *shim.ChaincodeStub, function string, args []
 // Query callback representing the query of a chaincode
 func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Printf("Query called, determining function")
-	
+
 	if function != "query" {
 		fmt.Printf("Function is query")
 		return nil, errors.New("Invalid query function name. Expecting \"query\"")
