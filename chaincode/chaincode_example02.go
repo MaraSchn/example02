db003d276646a55fcae2485e4182bfe4988bcb41
Philipp Rottner <xy0815man+bitbucket@googlemail.com>
2017-08-11 20:31:04 +0200

    fixed used „“

--- chaincode/chaincode_example02.go
+++ chaincode/chaincode_example02.go
@@ -89,11 +89,11 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	account = Account{}
 
 	// fill account template with values read from blockchain
-	json.Unmarshal(t.Query(stub, “query“, args[0]), &account)
+	json.Unmarshal(t.Query(stub, "query", args[0]), &account)
 
 	fmt.Printf("account %d has balance %s", args[0], account.balance_brutto)
 
-	json.Unmarshal(t.Query(stub, “query“, args[2]), &account)
+	json.Unmarshal(t.Query(stub, "query", args[2]), &account)
 
 	fmt.Printf("account %d has balance %s", args[2], account.balance_brutto)
 
